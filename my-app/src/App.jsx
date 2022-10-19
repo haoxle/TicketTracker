13649a3637411c7a9209b094ed488b2d48708cdb
haoxle <hao-le@live.co.uk>
2022-10-19 08:54:21 +0100

    styling semifixed?

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -9,18 +9,17 @@ import "./fonts/Oskosh.ttf";
 const App = () => {
   return (
     <>
-      <div class="sub-container">
-        <div class="sky">
-          <div class="stars"></div>
-          <div class="stars2"></div>
-          <div class="stars3"></div>
-          <div class="comet"></div>
-        </div>
-      </div>
       <div className="title">
-        <h1>Ticket Tracker</h1>
-
+        <h1 className="title-header">Ticket Tracker</h1>
         <Ticket />
+        <div className="sub-container">
+          <div className="sky">
+            <div className="stars"></div>
+            <div className="stars2"></div>
+            <div className="stars3"></div>
+            <div className="comet"></div>
+          </div>
+        </div>
       </div>
     </>
   );
